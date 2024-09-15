9d541b8c23309e8d1c3d05ba102f612412aaff09
shaoyuliu98 <shaoyuliu98@gmail.com>
2024-09-15 12:49:51 -0400

    trend following strat update

--- signals/cta_momentum_etf.py
+++ signals/cta_momentum_etf.py
@@ -1,14 +1,19 @@
+import sys
+
+sys.path.append('../')
+
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from performance import Trading_Strategy
+from performance_metrics.performance import Trading_Strategy
+from performance_metrics import price_to_info as pti
 import pytz
-import matplotlib.pyplot as plt
 import matplotlib
-import price_to_info as pti
+import matplotlib.pyplot as plt
 import warnings
 
 warnings.filterwarnings('ignore')
+
 matplotlib.use("Qt5Agg")
 
 
@@ -29,10 +34,10 @@ class Momentum:
         self.monthly_log_return = pti.get_log_return(self.pivot_price_data, rtype='m')
         self.monthly_log_return_std = pti.rolling_std(self.daily_log_return, rtype='m')
 
-        self.signal_generation(momentum_dict,long_only_dict)
+        self.signal_generation(momentum_dict, long_only_dict)
         self.vol_adj()
 
-    def signal_generation_helper(self,ticker,momentum,longonly):
+    def signal_generation_helper(self, ticker, momentum, longonly):
         rolling_ret = {'rolling' + str(i): self.monthly_log_return[ticker].rolling(i).sum().dropna() for i in momentum}
         index_adj = rolling_ret['rolling' + str(momentum[-1])].index
         rolling_ret_adj = dict(zip(rolling_ret.keys(),
@@ -44,31 +49,36 @@ class Momentum:
         else:
             avg_rolling_ret['signal'] = np.where(avg_rolling_ret[ticker] >= 0, 1, -1)
 
-        return avg_rolling_ret[['signal']].rename({'signal':ticker},axis=1)
+        return avg_rolling_ret[['signal']].rename({'signal': ticker}, axis=1)
 
     def signal_generation(self, momentum_dict, long_only_dict):
-        if momentum_dict is None:
-            momentum = {i:[3, 6, 9, 12] for i in self.tickers}
+        momentum = {}
+        if (momentum_dict is None):
+            momentum = {i: [3, 6, 9, 12] for i in self.tickers}
         else:
-            momentum = {}
-            for i,j in momentum_dict.items():
+            for i, j in momentum_dict.items():
                 for k in i:
                     momentum[k] = j
-
-            long_only = {}
-            for i,j in long_only_dict.items():
-                for k in i:
-                    long_only[k] = j
-
+                    
             for i in self.tickers:
                 if i not in momentum:
                     momentum[i] = [3, 6, 9, 12]
+
+        long_only = {}
+        if (long_only_dict is None):
+            long_only = {i: False for i in self.tickers}
+        else:
+            for i, j in long_only_dict.items():
+                for k in i:
+                    long_only[k] = j
+                    
+            for i in self.tickers:
                 if i not in long_only:
                     long_only[i] = False
 
         all_signal = pd.DataFrame()
         for i in momentum:
-            all_signal = pd.concat([all_signal,self.signal_generation_helper(i, momentum[i], long_only[i])],axis=1)
+            all_signal = pd.concat([all_signal, self.signal_generation_helper(i, momentum[i], long_only[i])], axis=1)
 
         self.signals = all_signal.copy().dropna()
         for i in self.signals.columns:
@@ -82,7 +92,7 @@ class Momentum:
 
         return
 
-    def get_performance(self,allocation):
+    def get_performance(self, allocation):
         sub_signals = self.signals[allocation.columns]
         position = allocation * sub_signals
 
@@ -101,29 +111,29 @@ class Momentum:
 
 if __name__ == '__main__':
     etf_list = [
-                'SPY', 'IWM', 'EFA', 'EEM', 'QQQ',
-                'XLE', 'XLK', 'XLY', 'IYR', 'SMH',
-                # 'LQD', 'IEF', 'TIP', 'TLT', 'AGG',
-                # 'UUP', 'FXE', 'FXF'
-                'GLD', 'USO', 'DBC', 'SLV', 'UNG', 'CORN',
-                ]
+        'SPY', 'IWM', 'EFA', 'EEM', 'QQQ',
+        'XLE', 'XLK', 'XLY', 'IYR', 'SMH',
+        # 'LQD', 'IEF', 'TIP', 'TLT', 'AGG',
+        # 'UUP', 'FXE', 'FXF'
+        'GLD', 'USO', 'DBC', 'SLV', 'UNG', 'CORN',
+    ]
 
-    stock_etf = ['SPY', 'IWM', 'EFA', 'EEM', 'QQQ'] # s&p, russell2000, eu/japan/aus, emerging market, nasdaq
-    sector_etf = ['XLE', 'XLK', 'XLY', 'IYR', 'SMH'] # energy, tech, consumer discretionary, real estate, semi
+    stock_etf = ['SPY', 'IWM', 'EFA', 'EEM', 'QQQ']  # s&p, russell2000, eu/japan/aus, emerging market, nasdaq
+    sector_etf = ['XLE', 'XLK', 'XLY', 'IYR', 'SMH']  # energy, tech, consumer discretionary, real estate, semi
     # bond_etf = ['LQD', 'IEF', 'TIP', 'TLT','AGG'] # IG corp, 7-10yr treasury, inflation bond, 20+yr treasury, IG corp market
     # fx_etf = ['UUP', 'FXE', 'FXF'] # US $ bull, Euro, Swiss Franc
-    commod_etf = ['GLD', 'USO', 'DBC', 'SLV', 'UNG', 'CORN'] # gold, oil, commod index, silver, natural gas, corn
+    commod_etf = ['GLD', 'USO', 'DBC', 'SLV', 'UNG', 'CORN']  # gold, oil, commod index, silver, natural gas, corn
 
     # etf_list = list(set(etf_list)-set(fx_etf))
 
     tz = pytz.timezone('America/New_York')
     st = tz.localize(datetime(2006, 1, 1))
-    et = tz.localize(datetime(2024, 8, 1))
+    et = tz.localize(datetime(2024, 9, 1))
 
-    filename = './data/ETF for Momentum.csv'
+    filename = '../yfinance_data/cta_momentum_data.csv'
 
     momentum_window = {tuple(stock_etf): [3, 6, 9, 12],
-                       tuple(sector_etf):[3, 6, 9, 12],
+                       tuple(sector_etf): [3, 6, 9, 12],
                        # tuple(bond_etf): [3, 6, 9, 12],
                        # tuple(fx_etf): [3, 6, 9, 12],
                        tuple(commod_etf): [3, 6, 9, 12]}
@@ -146,11 +156,12 @@ if __name__ == '__main__':
               # bond_etf,
               # fx_etf,
               commod_etf]:
-        temp_return = momentum_strat.get_performance(pti.get_alloc_weight(momentum_strat.get_vol(),i))
-        all_return = pd.concat([all_return,temp_return.set_index('date')],axis=1)
+        temp_return = momentum_strat.get_performance(pti.get_alloc_weight(momentum_strat.get_vol(), i))
+        all_return = pd.concat([all_return, temp_return.set_index('date')], axis=1)
 
-        temp_perf = Trading_Strategy(temp_return.loc[temp_return['date'] >= strategy_cutoff], 'Monthly').get_metrics_table()
-        all_return_perf = pd.concat([all_return_perf,temp_perf],axis=1)
+        temp_perf = Trading_Strategy(temp_return.loc[temp_return['date'] >= strategy_cutoff],
+                                     'Monthly').get_metrics_table()
+        all_return_perf = pd.concat([all_return_perf, temp_perf], axis=1)
 
     all_return.columns = ['overall',
                           'stocks',
@@ -172,8 +183,8 @@ if __name__ == '__main__':
     print('\n')
     # print(all_return[strategy_cutoff:].corr())
     # print('\n')
-    print(momentum_strat.position_no_shift.iloc[-1])
+    # print(momentum_strat.position_no_shift.iloc[-1])
 
     # print(all_return.rolling(12).corr().iloc[-4:,:])
-    # np.exp(all_return[strategy_cutoff:].cumsum()).plot()
-    # plt.show()
+    np.exp(all_return[strategy_cutoff:].cumsum()).plot()
+    plt.show()
